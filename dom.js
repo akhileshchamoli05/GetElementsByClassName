1ba3fc4a286cec1efb5f18d72b0886b1e21e8efa
akhileshchamoli05 <133146298+akhileshchamoli05@users.noreply.github.com>
2023-05-14 22:24:20 +0530

    DOM GetElementByTagName

---
+++ dom.js
@@ -0,0 +1,12 @@
+/*var items=document.getElementsByClassName("list-group-item");
+console.log(items);
+items[1].textContent='Hello 2';
+items[1].style.fontweight='bold';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++)
+{
+    items.style.fontweight='bold';
+}*/
+
+var tags=document.getElementsByTagName("li");
+tags[1].style.backgroundColor="Yellow";
